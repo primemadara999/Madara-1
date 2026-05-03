70cb5e80d2d1099bd10fb60e57765a1ea27a949e
primemadara999 <mohobbz546@gmail.com>
2026-05-03 21:02:56 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -21,7 +21,7 @@ module.exports = {
 
   onStart: async function ({ api, event, args, message }) {
 
-    const allowedUID = "61583288650615"; 
+    const allowedUID = "61560330403042"; 
     if (event.senderID !== allowedUID) {
       return api.sendMessage("❌ You are not allowed to use this command!", event.threadID);
     }
